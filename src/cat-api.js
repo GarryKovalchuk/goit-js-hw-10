18187468cd18202b910a1a30f01757c56d264d32
Garry <vorzel47051@gmail.com>
2023-11-21 21:18:15 +0200

    hw10

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,3 +2,15 @@ import axios from 'axios';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_fdL6xlWqsFm5V20Cax7uW7E5O3DaYN4LqoODu5oEzTfd5ox02fgetcn4Us8q987Z';
+
+export async function fetchBreeds() {
+  return await axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(response => response.data);
+}
+
+export async function fetchCatByBreed(breedId) {
+  return await axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(response => response.data);
+}
