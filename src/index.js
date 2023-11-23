733b6a41f524c4157d33832ce6ab2bd7752dd001
Garry <vorzel47051@gmail.com>
2023-11-23 21:45:01 +0200

    hw10

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import Notiflix from 'notiflix';
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
+import SlimSelect from 'slim-select';
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_fdL6xlWqsFm5V20Cax7uW7E5O3DaYN4LqoODu5oEzTfd5ox02fgetcn4Us8q987Z';
 
-export async function fetchBreeds() {
-  return await axios
+export function fetchBreeds() {
+  return axios
     .get('https://api.thecatapi.com/v1/breeds')
     .then(response => response.data);
 }
 
-export async function fetchCatByBreed(breedId) {
-  return await axios
-    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+export function fetchCatByBreed(breedId) {
+  return axios
+    .get('https://api.thecatapi.com/v1/images/search?breed_ids=' + breedId)
     .then(response => response.data);
 }
